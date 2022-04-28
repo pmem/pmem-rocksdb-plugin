2ff57b328eca5fffcdf127c5cca73dd481306295
Chen, You <you.chen@intel.com>
2022-04-28 22:12:11 +0800

    fix to use lru cache index

--- plugin/pmem_scache/scache/pmem_scache.cc
+++ plugin/pmem_scache/scache/pmem_scache.cc
@@ -4,59 +4,108 @@
 #include "pmem_scache.h"
 
 #include <climits>
-#include <memory>
 
 #include "rocksdb/utilities/object_registry.h"
+#include "util/compression.h"
 namespace ROCKSDB_NAMESPACE {
 PMemSecondaryCache::PMemSecondaryCache(const PMemAllocatorOptions& opt)
-    : option_(opt),
-      insert_queue_(1LL * 1024 * 1024 * 1024),
-      insert_thread_(InsertMain, this) {
+    : option_(opt) {
   if (option_.is_kmem_dax) {
     allocator_ = std::make_shared<KMemAllocator>();
   } else {
     allocator_ = std::make_shared<FSDaxAllocator>(opt.pmem_path, opt.max_size);
   }
-  ecache_ptr = std::make_shared<ECache>(opt.max_size, allocator_);
+  cache_ = NewLRUCache(opt.max_size * 0.8, 6, true, 0.0, allocator_,
+                       kDefaultToAdaptiveMutex, kDontChargeCacheMetadata);
 }
 
-Status PMemSecondaryCache::Insert(const Slice& key, void* value,
-                                  const Cache::CacheItemHelper* helper) {
+Status PMemSecondaryCache::Insert(const Slice& key, void* value, const Cache::CacheItemHelper* helper) {
   size_t size = (*helper->size_cb)(value);
-  char* buf = new char[size + sizeof(uint64_t)];
+  std::unique_ptr<char> buf_ptr(new char[size + sizeof(uint64_t)]);
+  char* buf = buf_ptr.get();
   EncodeFixed64(buf, size);
   Status s = (*helper->saveto_cb)(value, 0, size, buf + sizeof(uint64_t));
-  if (!s.ok()) {
-    delete[] buf;
+  if(!s.ok()) {
     fprintf(stderr, "helper_cb failed to extract value\n");
     return s;
   }
-  insert_queue_.Push(InsertOp(key, buf, size + sizeof(uint64_t)));
+
+  CompressionOptions compression_opts;
+  CompressionContext compression_context(kSnappyCompression);
+  uint64_t sample_for_compression = 0;
+  CompressionInfo compression_info(
+      compression_opts, compression_context, CompressionDict::GetEmptyDict(),
+      kSnappyCompression, sample_for_compression);
+  Slice vslice(buf, size + sizeof(uint64_t));
+
+  std::string output;
+  bool success = CompressData(vslice, compression_info, -1, &output);
+  if(!success) {
+    fprintf(stderr, "failed to compress\n");
+    return Status::IOError("failed to compress");
+  }
+
+  auto* entry = new CacheEntry(allocator_);
+  entry->data = (char*)allocator_->Allocate(output.size());
+  entry->size = output.size();
+  if(entry->data == nullptr) {
+    delete entry;
+    // still return OK
+    return Status::OK();
+  }
+  pmem_memcpy(entry->data, output.c_str(), output.size(), PMEM_F_MEM_NONTEMPORAL);
+
+  s = cache_->Insert(key, entry, output.size(),
+                                     [](const Slice& /*key*/, void* val) -> void {
+                                       delete static_cast<CacheEntry*>(val);
+                                     });
+  if(!s.ok()) {
+    fprintf(stderr, "internal lru cache failed to insert entry\n");
+  }
   return Status::OK();
 }
 
-void PMemSecondaryCache::InsertMain(PMemSecondaryCache* s_cache) {
-  while (true) {
-    InsertOp op(s_cache->insert_queue_.Pop());
-    if (op.signal_) {
-      // that is a secret signal to exit
-      break;
+std::unique_ptr<SecondaryCacheResultHandle> PMemSecondaryCache::Lookup(
+    const Slice& key, const Cache::CreateCallback& create_cb,
+    bool /*wait*/) {
+  std::string key_str = key.ToString();
+
+  std::unique_ptr<SecondaryCacheResultHandle> secondary_handle;
+
+  Cache::Handle* handle = cache_->Lookup(key);
+  if (handle) {
+    void* value = nullptr;
+    size_t charge = 0;
+    Status s;
+    auto* cvalue = (CacheEntry*)cache_->Value(handle);
+    size_t csize = cvalue->size;
+    const char* cdata = (const char*)cvalue->data;
+
+    UncompressionContext uncompressionContext(kSnappyCompression);
+    UncompressionInfo uncompressionInfo(uncompressionContext, UncompressionDict::GetEmptyDict(), kSnappyCompression);
+    size_t uncompress_size;
+    std::string output;
+    CacheAllocationPtr uncompress_ptr = UncompressData(uncompressionInfo, cdata, csize, &uncompress_size, -1);
+    char* uncompress_raw = uncompress_ptr.get();
+
+    if(!uncompress_ptr) {
+      fprintf(stderr, "failed to decompress\n");
+      cache_->Release(handle);
+      return secondary_handle;
     }
-    Slice value(op.data_.get(), op.data_size_);
-    int retry = 0;
-    while ((s_cache->ecache_ptr->InsertImpl(op.key_, value)).IsTryAgain()) {
-      if (retry > kMaxRetry) {
-        break;
-      }
-      retry++;
+
+    size_t size = DecodeFixed64(uncompress_raw);
+    uncompress_raw += sizeof(uint64_t);
+    s = create_cb(uncompress_raw, size, &value, &charge);
+    if (s.ok()) {
+      secondary_handle.reset(
+          new PMemSCacheResultHandle(cache_.get(), handle, value, charge));
+    } else {
+      cache_->Release(handle);
     }
   }
-}
-void PMemSecondaryCache::Close() {
-  if (insert_thread_.joinable()) {
-    insert_queue_.Push(InsertOp(true));
-    insert_thread_.join();
-  }
+
+  return secondary_handle;
 }
 
 extern "C" FactoryFunc<SecondaryCache> pmem_scache_reg;
